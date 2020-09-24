64e051b8022a4c4d07ba84ab7c7ffb6d28a4ce55
Ivan Villareal <ivaano@gmail.com>
2020-09-23 20:28:51 -0700

    Clean up, adding Instructions and requirements.txt

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # bitbucket-repo-downloader
-Clone all the  user bitbucket repositories
+Clone/Update all  user repositories from bitbucket.
 
+###Requirements
+* python3.6+
+* git
+* ssh
 
-Python 3.7 needed
+###Installation
+* Because the clone is done using ssh, you need to add your ssh key to bitbucket (https://bitbucket.org/account/settings/ssh-keys/)
+* Clone this repo ```git clone https://github.com/ivaano/bitbucket-repo-downloader.git```
+* Create a new virtual environment ```python3.7 -m venv venv```
+* Activate virtual environment ```source venv/bin/activate```
+* Go to project directory ```cd bitbucket-repo-downloader```
+* Install dependencies ```pip install -r requirements.txt```
+* Copy `config/config.sample` to `config/config.ini` and put your settings there.
 
+###Usage
+If you have set your settings in the config.ini file
+just run the program ```python main.py```
+Alternatively you can run the interactive mode by adding the `python main.py -i` argument
+and it will ask for user/password workspace and destination dir.
 
-Instructions later
--- main.py
+++ main.py
@@ -37,21 +37,28 @@ def update_repo(url: str, destination: str):
         print("Updated %s to %s" % (fetch_info.ref, fetch_info.commit))
 
 
-def clone_repo_list(repo_list: list, dest_path: str):
+def clone_repo_list(repo_list: list, clone_protocol: str, destination_path: str) -> dict:
+    stats = {'updated' : 0, 'updated_fail': 0, 'cloned': 0, 'cloned_fail': 0}
     for repo in repo_list:
-        dest_path = os.path.join(dest_path, repo.get('project').get('name'), repo.get('name'))
+        dest_path = os.path.join(destination_path, repo.get('project').get('name'), repo.get('name'))
+        repo_address = repo.get('links').get('clone')[0].get('href') if clone_protocol == 'https' else repo.get('links').get('clone')[1].get('href')
         if os.path.isdir(dest_path):
             try:
-                update_repo(repo.get('links').get('clone')[1].get('href'), dest_path)
+                update_repo(repo_address, dest_path)
+                stats['updated'] = stats['updated'] + 1
             except:
                 logging.warning('Repo {} had some problems during cloning'.format(repo.get('name')))
+                stats['updated_fail'] = stats['fail'] + 1
                 pass
         else:
             try:
-                clone_repo(repo.get('links').get('clone')[1].get('href'), dest_path)
+                clone_repo(repo_address, dest_path)
+                stats['cloned'] = stats['cloned'] + 1
             except:
                 logging.warning('Repo {} had some problems during cloning'.format(repo.get('name')))
+                stats['cloned_fail'] = stats['cloned_fail'] + 1
                 pass
+    return stats
 
 
 def get_all_repos(client: Client, workspace: str = None) -> list:
@@ -84,12 +91,14 @@ def read_config():
 def main(interactive: bool):
     conf = read_config()
     bb = None
+    clone_protocol = 'ssh'
     style = Style.from_dict(
         {
-            "input": "#f97c16",
+            "input": "#89C713",
+            "output": "#132FC7",
             "normal": "#fcfcfc",
             "warn": "#ff0066",
-            "info": "#00ffa1",
+            "info": "#1389C7",
         }
     )
 
@@ -112,22 +121,20 @@ def main(interactive: bool):
             if not os.path.isdir(destination_path):
                 raise DestinationPathError
         except NotAuthenticatedError:
-            print = print_formatted_text
             text_fragments = FormattedText([
                     ("class:warn", "Invalid Credentials... ")])
-            print(text_fragments, style=style)
+            print_formatted_text(text_fragments, style=style)
+
             exit(1)
         except NotFoundError:
-            print = print_formatted_text
             text_fragments = FormattedText([
                     ("class:warn", "Invalid Workspace... ")])
-            print(text_fragments, style=style)
+            print_formatted_text(text_fragments, style=style)
+
             exit(1)
     else:
-        text_fragments = FormattedText([("class:input", "bitbucket user: ")])
-        user = prompt(text_fragments, style=style)
-        text_fragments = FormattedText([("class:input", "bitbucket password: ")])
-        password = prompt(text_fragments, style=style, is_password=True)
+        user = prompt(FormattedText([("class:input", "bitbucket user: ")]), style=style)
+        password = prompt(FormattedText([("class:input", "bitbucket password: ")]), style=style, is_password=True)
         try:
             bb = Client(user=user, password=password)
             workspaces = get_workspaces(bb)
@@ -142,32 +149,35 @@ def main(interactive: bool):
             workspace = prompt(text_fragments, style=style)
             if workspace not in slugs:
                 raise NotFoundError
-            text_fragments = FormattedText([("class:input", "destination path: ")])
-            destination_path = prompt(text_fragments, style=style)
+            destination_path = prompt(FormattedText([("class:input", "destination path: ")]), style=style)
             if not os.path.isdir(destination_path):
                 raise DestinationPathError
+            #clone_protocol = prompt(FormattedText([("class:input", "clone protocol (*ssh, https): ")]), default='ssh', style=style)
+            print_formatted_text(FormattedText([("class:input", "clone protocol (*ssh, https): ssh")]), style=style)
         except NotAuthenticatedError:
-            print = print_formatted_text
             text_fragments = FormattedText([
                     ("class:warn", "Invalid Credentials... ")])
-            print(text_fragments, style=style)
+            print_formatted_text(text_fragments, style=style)
             exit(1)
         except NotFoundError:
-            print = print_formatted_text
-            text_fragments = FormattedText([
-                    ("class:warn", "Invalid Workspace... ")])
-            print(text_fragments, style=style)
+            print_formatted_text(FormattedText([("class:warn", "Invalid Workspace... ")]), style=style)
+
             exit(1)
         except DestinationPathError:
-            print = print_formatted_text
-            text_fragments = FormattedText([
-                    ("class:warn", "Invalid Destination path, it must be a directory that exists. ")])
-            print(text_fragments, style=style)
+            print_formatted_text(FormattedText([("class:warn", "Invalid Destination path, it must be a directory that exists. ")]), style=style)
             exit(1)
 
     logging.info('Starting')
     repos = get_all_repos(bb, workspace)
-    clone_repo_list(repos, destination_path)
+    stats = clone_repo_list(repos, clone_protocol, destination_path)
+    print_formatted_text(FormattedText([("class:info", "========= Stats =========")]), style=style)
+    for key, value in stats.items():
+        text_fragments = FormattedText([
+            ("class:input", key),
+            ("class:input", ': '),
+            ("class:info", str(value)),
+        ])
+        print_formatted_text(text_fragments, style=style)
     logging.info('End')
 
 
